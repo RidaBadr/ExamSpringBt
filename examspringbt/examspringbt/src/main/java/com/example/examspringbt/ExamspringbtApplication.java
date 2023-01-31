2f2600157a6313d387fea622a0c12bcab04ae2b9
RidaBadr <114596641+RidaBadr@users.noreply.github.com>
2023-01-31 09:53:31 +0100

    first commit

---
+++ examspringbt/examspringbt/src/main/java/com/example/examspringbt/ExamspringbtApplication.java
@@ -0,0 +1,13 @@
+package com.example.examspringbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamspringbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamspringbtApplication.class, args);
+	}
+
+}
---
+++ examspringbt/examspringbt/src/test/java/com/example/examspringbt/ExamspringbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examspringbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamspringbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
